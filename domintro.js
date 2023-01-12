5bdfe3c8302b17ad6e531ac6aae7f2c8e6520169
shubh <shubhamdixit863@gmail.com>
2023-01-12 20:25:26 +0530

    Dom element by id

---
+++ domintro.js
@@ -0,0 +1,22 @@
+// window object
+
+//console.log(window); //its a global object provided to you by the browser ,giving you lot of extra utilities
+// even your dom is the part of window object
+
+// How to use dom  ,
+
+//Getting elements by id  from th edom object
+//document.getElementById
+
+const heading=document.getElementById("heading");
+//console.log(heading);  // this object will contain all the css properties all the text properties and you can manipulate them
+
+//heading.style.color="green";
+/*
+
+document.getElementById("btn").addEventListener("click",function(){
+ heading.style.color="brown";
+
+heading.innerText="Something else"
+})
+*/
