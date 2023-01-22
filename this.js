5480f32149da8d742198a13c6d3ce5fcab833ea5
shubh <shubhamdixit863@gmail.com>
2023-01-22 12:58:18 +0530

    this value inside the enclosing function

--- this.js
+++ this.js
@@ -50,5 +50,36 @@ obj.foo();
 // Rules for remembering this keyword
 // 1- If we have es5 function then the value of this is window object or the object to which the function is attached
 
-// 2 - in case of arrow function no matter what you do the value of this would be the window object 
 
+
+
+
+const obj7 = {
+    name: 'jhon',
+    func: function() {
+      // the value of this inside here would be the object that you have
+      const obj2 = {
+        surname: 'something',
+        func2: () => { 
+          console.log(this); 
+        
+        },
+  
+        func1:function(){
+            console.log(this); 
+        }
+       
+      }   
+       obj2.func1() 
+       obj2.func2() 
+  }
+  }
+    
+  obj7.func()
+  
+  
+
+  // 2 - in case of arrow functions the value of this is relative to the value of this inside  enclosing function 
+
+  // it may be the window object if the enclosing function has the value of this as window object 
+  // or the other wise the object to which the enclosing function is bound to
